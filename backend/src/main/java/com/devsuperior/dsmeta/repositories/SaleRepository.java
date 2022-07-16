b7061904df209628087e08b0358019c3e387e8a1
Viviane dos Santos Correa <vivianecorreasv@gmail.com>
2022-07-15 22:52:16 -0300

    API test

---
+++ backend/src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,10 @@
+package com.devsuperior.dsmeta.repositories;
+
+import com.devsuperior.dsmeta.entities.Sale;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SaleRepository extends JpaRepository <Sale, Long> {
+}
+
+
+
